1f673e889c4c63139dfdc9cc5185b639dd100ecc
vchikalkin <djchikalkin@gmail.com>
2026-06-27 08:19:22 +0300

    chore: add eslint-config-sheriff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { sheriff } from "eslint-config-sheriff";
+import { defineConfig } from "eslint/config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const sheriffOptions = {
+  // files: ["src/**/*"], // Only the files in the src directory will be linted.
+  react: true,
+  next: true,
+  astro: false,
+  lodash: false,
+  remeda: false,
+  playwright: false,
+  storybook: false,
+  jest: false,
+  vitest: false,
+  tsconfigRootDir: import.meta.dirname,
+};
 
-export default eslintConfig;
+export default defineConfig(sheriff(sheriffOptions));
